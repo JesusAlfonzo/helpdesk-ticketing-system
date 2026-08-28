47a16180986c7a66681af43ce3d93b02f4e9d6be
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-08-28 00:26:55 -0400

    refactor: Reorganizar rutas en api.php para mejorar la legibilidad

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -7,29 +7,16 @@ use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum'); // Obtener el usuario autenticado
 
-Route::get('/tickets', [
-    TicketController::class,
-    'index'
-]);
+Route::get('/tickets', [TicketController::class, 'index']); // Listar todos los tickets
 
-Route::post('/tickets', [
-    TicketController::class,
-    'store'
-]);
+Route::post('/tickets', [TicketController::class, 'store']); // Crear un nuevo ticket
 
-Route::get('/technicians', [
-    UserController::class,
-    'index'
-]);
+Route::delete('/tickets/{ticket}', [TicketController::class, 'destroy']); // Eliminar un ticket
 
-Route::patch('/tickets/{ticket}/assign', [
-    TicketController::class,
-    'assignTicket'
-]);
+Route::get('/technicians', [UserController::class, 'index']); // Listar todos los técnicos
 
-Route::patch('/tickets/{ticket}/status', [
-    TicketController::class,
-    'updateStatus'
-]);
+Route::patch('/tickets/{ticket}/assign', [TicketController::class, 'assignTicket']); // Asignar un ticket a un técnico
+
+Route::patch('/tickets/{ticket}/status', [TicketController::class, 'updateStatus']); // Actualizar el estado de un ticket
