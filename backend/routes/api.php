ea5a4a6cf063e7f42a61ebb8771833fb0bb61bff
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2026-08-27 22:20:01 -0400

    feat: Agregar rutas para obtener técnicos y asignar tickets

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,4 +17,14 @@ Route::get('/tickets', [
 Route::post('/tickets', [
     TicketController::class,
     'store'
+]);
+
+Route::get('/technicians', [
+    UserController::class,
+    'index'
+]);
+
+Route::patch('/tickets/{ticket}/assign', [
+    TicketController::class,
+    'assignTicket'
 ]);
